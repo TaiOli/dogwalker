26870ff9f31980104ef62fee05e9b4c4fa6d83da
Tais <taiscoliveiratec@gmail.com>
2026-06-20 16:43:18 -0300

    feat: adiciona funcao login no controller

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -17,6 +18,22 @@ class UserController extends Controller
 
         return response()->json([
             'message' => 'Usuário criado com sucesso',
+            'user' => $user
+        ]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Credenciais inválidas'
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'Login realizado com sucesso',
             'user' => $user
         ]);
     }
