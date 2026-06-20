1acdb077b5cd125b1607128bfb21e67bc185c89c
Tais <taiscoliveiratec@gmail.com>
2026-06-20 12:16:48 -0300

    feat: adiciona campos do request no controller

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,20 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function store(StoreUserRequest $request)
+    public function store(Request $request)
     {
-        $data = $request->validated();
-
-        $data['password'] = Hash::make($data['password']);
-
-        $user = User::create($data);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'nome' => $request->nome,
+            'telefone' => $request->telefone,
+            'tipo_usuario' => $request->tipo_usuario,
+            'foto' => $request->foto,
+        ]);
 
         return response()->json([
             'message' => 'Usuário criado com sucesso',
