a240443296c2a6efd2b5742e7ac3b473c4b0588f
Tais <taiscoliveiratec@gmail.com>
2026-06-20 12:21:07 -0300

    adiciona tabela de dogs

---
+++ backend/database/migrations/2026_06_20_151756_create_dogs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dogs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('nome');
+            $table->integer('idade')->nullable();
+            $table->string('porte');
+            $table->string('raca')->nullable();
+            $table->string('foto')->nullable();
+            $table->text('observacoes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dogs');
+    }
+};
