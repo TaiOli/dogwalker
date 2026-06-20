921ec8b15dc739e34a539cf4fc9f952e6b109ded
Tais <taiscoliveiratec@gmail.com>
2026-06-20 18:31:47 -0300

    refactor:login e cadastro em useauth

---
+++ frontend/src/composables/userAuth.js
@@ -0,0 +1,49 @@
+import { reactive } from "vue";
+import { api } from "../services/api";
+
+export function useAuth() {
+
+  const formLogin = reactive({
+    email: "",
+    password: ""
+  });
+
+  async function login() {
+    const res = await api.post("/login", formLogin);
+    return res.data;
+  }
+
+  function clearLogin() {
+    formLogin.email = "";
+    formLogin.password = "";
+  }
+
+  const formCadastro = reactive({
+    name: "",
+    nome: "",
+    email: "",
+    password: ""
+  });
+
+  async function cadastrar() {
+    const res = await api.post("/users", formCadastro);
+    return res.data;
+  }
+
+  function clearCadastro() {
+    formCadastro.name = "";
+    formCadastro.nome = "";
+    formCadastro.email = "";
+    formCadastro.password = "";
+  }
+
+  return {
+    formLogin,
+    login,
+    clearLogin,
+
+    formCadastro,
+    cadastrar,
+    clearCadastro
+  };
+}
