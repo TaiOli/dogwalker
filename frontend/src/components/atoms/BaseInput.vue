9859f74fab821989b726b5239a32e2e9d789bd11
Tais <taiscoliveiratec@gmail.com>
2026-06-20 19:32:02 -0300

    refactor:adiciona atoms para inputs

---
+++ frontend/src/components/atoms/BaseInput.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+    modelValue:String,
+    placeholder:String,
+    type:{
+        type:String,
+        default:"text"
+    }
+});
+
+defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+    <input
+        :value="modelValue"
+        :placeholder="placeholder"
+        :type="type"
+        @input="$emit('update:modelValue', $event.target.value)"
+    />
+</template>
+
+<style scoped>
+input {
+  padding: 12px;
+  border: 1px solid #ddd;
+  border-radius: 6px;
+  outline: none;
+  background: white;
+}
+
+input:focus {
+  border-color: #42b983;
+}
+</style>
