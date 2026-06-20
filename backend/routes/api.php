10aaf50c4e2ef5f856babd3dc3d1943836d96047
Tais <taiscoliveiratec@gmail.com>
2026-06-20 12:42:02 -0300

    feat:adiciona rota para passeios

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DogController;
+use App\Http\Controllers\PasseioController;
 
 Route::post('/users', [UserController::class, 'store']);
 Route::post('/dogs', [DogController::class, 'store']);
+Route::post('/passeios', [PasseioController::class, 'store']);
