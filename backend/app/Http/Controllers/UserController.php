1fbc23cd10e409d14519ea773ccd3962bed2ad69
Tais <taiscoliveiratec@gmail.com>
2026-06-20 13:36:00 -0300

    feat: utiliza StoreUserRequest para validacao de user

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,22 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
+            ...$request->validated(),
             'password' => Hash::make($request->password),
-            'nome' => $request->nome,
-            'telefone' => $request->telefone,
-            'tipo_usuario' => $request->tipo_usuario,
-            'foto' => $request->foto,
         ]);
 
         return response()->json([
