43217b4f9c4ec8f77f1646110e527d6383995aee
Tais <taiscoliveiratec@gmail.com>
2026-06-20 19:31:36 -0300

    refactor:adiciona atoms para botao

---
+++ frontend/src/components/atoms/BaseButton.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+    label:String
+});
+
+defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+    <button>
+        {{ label }}
+    </button>
+</template>
+
+<style scoped>
+button {
+  width: 100%;
+  padding: 10px;
+  border: none;
+  background: #42b983;
+  color: white;
+  border-radius: 6px;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+button:hover {
+  background: #369b6f;
+}
+
+</style>
