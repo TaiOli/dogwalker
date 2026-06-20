069a49a8eb008f16b6378b34cf7d2e239130dfec
Tais <taiscoliveiratec@gmail.com>
2026-06-20 12:26:02 -0300

    adiciona rota para dogs

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DogController;
 
 Route::post('/users', [UserController::class, 'store']);
+Route::post('/dogs', [DogController::class, 'store']);
