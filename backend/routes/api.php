50c3bca6b97c8e3ca0b2d808288313ddfad19ffa
Tais <taiscoliveiratec@gmail.com>
2026-06-21 15:13:36 -0300

    feat: adiciona sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\DogController;
 use App\Http\Controllers\PasseioController;
 
 Route::post('/users', [UserController::class, 'store']);
-Route::post('/dogs', [DogController::class, 'store']);
-Route::post('/passeios', [PasseioController::class, 'store']);
 Route::post('/login', [UserController::class, 'login']);
+
+// Protegidas
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/dogs', [DogController::class, 'store']);
+    Route::post('/passeios', [PasseioController::class, 'store']);
+});
