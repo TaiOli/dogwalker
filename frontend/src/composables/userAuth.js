93fdfc00d2fb98b07f0d4ded951dcda3d4ddd540
Tais <taiscoliveiratec@gmail.com>
2026-06-21 14:21:21 -0300

    feat: adiciona visualizacao para usuarios de forma diferente

--- frontend/src/composables/userAuth.js
+++ frontend/src/composables/userAuth.js
@@ -1,7 +1,9 @@
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import { api } from "../services/api";
 
 export function useAuth() {
+
+  const user = ref(JSON.parse(localStorage.getItem("user")));
 
   const formLogin = reactive({
     email: "",
@@ -10,6 +12,10 @@ export function useAuth() {
 
   async function login() {
     const res = await api.post("/login", formLogin);
+
+    user.value = res.data.user;
+    localStorage.setItem("user", JSON.stringify(res.data.user));
+
     return res.data;
   }
 
@@ -38,17 +44,44 @@ export function useAuth() {
     formCadastro.nome = "";
     formCadastro.email = "";
     formCadastro.password = "";
-    formCadastro.tipo_usuario = "";
     formCadastro.telefone = "";
+    formCadastro.tipo_usuario = "tutor"; 
+    formCadastro.foto = "";
+  }
+
+  function obterUser() {
+    return user.value;
+  }
+
+  function obterFuncao() {
+    return user.value?.tipo_usuario;
+  }
+
+  function tutor() {
+    return obterFuncao() === "tutor";
+  }
+
+  function walker() {
+    return obterFuncao() === "passeador";
+  }
+
+  function logout() {
+    user.value = null;
+    localStorage.removeItem("user");
   }
 
   return {
     formLogin,
     login,
     clearLogin,
-
     formCadastro,
     cadastrar,
-    clearCadastro
+    clearCadastro,
+    user,
+    obterUser,
+    obterFuncao,
+    tutor,
+    walker,
+    logout
   };
 }
