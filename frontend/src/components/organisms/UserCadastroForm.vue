be6942c889caf915fc9ae2bbb7b20fedf7a0a083
Tais <taiscoliveiratec@gmail.com>
2026-06-21 11:51:19 -0300

    refactor:adiciona organism

---
+++ frontend/src/components/organisms/UserCadastroForm.vue
@@ -0,0 +1,22 @@
+<script setup>
+import BaseInput from "../atoms/BaseInput.vue";
+import BaseButton from "../atoms/BaseButton.vue";
+
+const props = defineProps({
+  form: Object,
+  labelButton: String
+});
+
+const emit = defineEmits(["submit"]);
+</script>
+
+<template>
+  <div class="form">
+    <BaseInput v-model="form.name" placeholder="Nome de usuário" />
+    <BaseInput v-model="form.nome" placeholder="Nome completo" />
+    <BaseInput v-model="form.email" placeholder="Email" />
+    <BaseInput v-model="form.password" placeholder="Senha" type="password" />
+
+    <BaseButton :label="labelButton" @click="emit('submit')" />
+  </div>
+</template>
