03bc0065938fa04a85dcb7c8ae855bcea3415cf8
Tais <taiscoliveiratec@gmail.com>
2026-06-21 11:51:01 -0300

    refactor:adiciona organism

---
+++ frontend/src/components/organisms/UserAuthForm.vue
@@ -0,0 +1,20 @@
+<script setup>
+import BaseInput from "../atoms/BaseInput.vue";
+import BaseButton from "../atoms/BaseButton.vue";
+
+const props = defineProps({
+  form: Object,
+  labelButton: String
+});
+
+const emit = defineEmits(["submit"]);
+</script>
+
+<template>
+  <div class="form">
+    <BaseInput v-model="form.email" placeholder="Email" />
+    <BaseInput v-model="form.password" placeholder="Senha" type="password" />
+
+    <BaseButton :label="labelButton" @click="emit('submit')" />
+  </div>
+</template>
