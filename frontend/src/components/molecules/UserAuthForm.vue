24f4bd8d674cac9fe7d097bebb0d4a50fde42340
Tais <taiscoliveiratec@gmail.com>
2026-06-21 10:42:35 -0300

    refactor: adiciona moleculas

---
+++ frontend/src/components/molecules/UserAuthForm.vue
@@ -0,0 +1,28 @@
+<script setup>
+import BaseButton from "../atoms/BaseButton.vue"
+import BaseInput from "../atoms/BaseInput.vue";
+
+defineProps({
+    form:Object,
+    labelButton: String
+});
+
+const emit = defineEmits(["submit"]);
+</script>
+
+<template>
+    <div class="form">
+        <BaseInput v-model="form.email" placeholder="Email" />
+        <BaseInput v-model="form.password" placeholder="Senha" type="password" />
+        <BaseButton :label="labelButton" @click="emit('submit')" />
+    </div>
+</template>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  margin: 20px 0;
+}
+</style>
