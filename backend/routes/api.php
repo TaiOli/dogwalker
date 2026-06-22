237d312e22e324889146123d95c7faf9761d6b0d
Tais <taiscoliveiratec@gmail.com>
2026-06-22 18:41:30 -0300

    feat: adiciona rota para tela de passeio e rota para busca do select na solicitacao do passeio

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,5 +11,9 @@ Route::post('/login', [UserController::class, 'login']);
 // Protegidas
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/dogs', [DogController::class, 'store']);
+    Route::get('/dogs/my', [DogController::class, 'myDogs']);
     Route::post('/passeios', [PasseioController::class, 'store']);
+    Route::get('/passeios', [PasseioController::class, 'index']);
+    Route::put('/passeios/{id}/aceitar', [PasseioController::class, 'aceitar']);
+    Route::put('/passeios/{id}/recusar', [PasseioController::class, 'recusar']);
 });
