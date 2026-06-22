35335c2b9a255abe48e01532fee4bdf630284d2d
Tais <taiscoliveiratec@gmail.com>
2026-06-22 14:47:00 -0300

    feat: adiciona cadastro de cachorro para tutores

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import User from "../views/User.vue";
 import Login from "../views/Login.vue";
 import Dashboard from "../views/Dashboard.vue";
+import CadastrarCachorro from "../views/RegisterDog.vue";
 
 const routes = [
   {
@@ -15,6 +16,10 @@ const routes = [
   {
     path: "/home",
     component: Dashboard
+  },
+  {
+    path: "/dogs",
+    component: CadastrarCachorro
   }
 ];
 
