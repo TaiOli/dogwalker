95fde1a91385313330c46a23f5ad67eae0f56b18
Tais <taiscoliveiratec@gmail.com>
2026-06-22 14:46:42 -0300

    feat: adiciona cadastro de cachorro para tutores

---
+++ frontend/src/components/molecules/DogForm.vue
@@ -0,0 +1,30 @@
+<script setup>
+import BaseInput from "../atoms/BaseInput.vue";
+import BaseButton from "../atoms/BaseButton.vue";
+
+defineProps({
+  form: Object,
+  labelButton: String
+});
+
+const emit = defineEmits(["submit"]);
+</script>
+
+<template>
+  <div class="form">
+    <BaseInput v-model="form.nome" placeholder="Nome do cachorro" />
+    <BaseInput v-model="form.idade" placeholder="Idade" type="number" />
+    <BaseInput v-model="form.porte" placeholder="Porte (pequeno, médio, grande)" />
+    <BaseInput v-model="form.raca" placeholder="Raça" />
+    <BaseInput v-model="form.observacoes" placeholder="Observações" />
+    <BaseButton :label="labelButton" @click="emit('submit')" />
+  </div>
+</template>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+}
+</style>
