4e75224b6c4159e5950f8ffe51ff0eba965f2e61
Tais <taiscoliveiratec@gmail.com>
2026-06-22 19:06:42 -0300

    feat:adiciona composable para agendar passeios

---
+++ frontend/src/composables/useScheduletour.js
@@ -0,0 +1,50 @@
+import { ref, reactive } from "vue"
+import { api } from "../services/api"
+
+export function useScheduletour() {
+
+  const dogs = ref([])
+
+  const form = reactive({
+    dog_id: "",
+    data: "",
+    hora: "",
+    duracao: "",
+    local: "",
+    valor: ""
+  })
+
+  async function loadDogs() {
+    try {
+
+      const res = await api.get("/dogs/my")
+
+      dogs.value = res.data
+    } catch (error) {
+      console.log("erro ao buscar dogs:", error.response?.data || error)
+    }
+  }
+
+  async function solicitarPasseio() {
+    return api.post("/passeios", form)
+  }
+
+  function clearPasseio() {
+    Object.assign(form, {
+      dog_id: "",
+      data: "",
+      hora: "",
+      duracao: "",
+      local: "",
+      valor: ""
+    })
+  }
+
+  return {
+    form,
+    dogs,
+    loadDogs,
+    solicitarPasseio,
+    clearPasseio
+  }
+}
