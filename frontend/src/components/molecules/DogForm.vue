e13504f20012779e8e690313141daf33b46018df
Tais <taiscoliveiratec@gmail.com>
2026-06-23 17:34:15 -0300

    feat: adiciona api publica

--- frontend/src/components/molecules/DogForm.vue
+++ frontend/src/components/molecules/DogForm.vue
@@ -12,12 +12,14 @@ const emit = defineEmits(["submit"]);
 
 <template>
   <div class="form">
-    <BaseInput v-model="form.nome" placeholder="Nome do cachorro" />
-    <BaseInput v-model="form.idade" placeholder="Idade" type="number" />
-    <BaseInput v-model="form.porte" placeholder="Porte (pequeno, médio, grande)" />
-    <BaseInput v-model="form.raca" placeholder="Raça" />
-    <BaseInput v-model="form.observacoes" placeholder="Observações" />
-    <BaseButton :label="labelButton" @click="emit('submit')" />
+    <BaseInput v-model="form.nome" placeholder="Nome do cachorro"/>
+    <BaseInput v-model="form.idade" placeholder="Idade" type="number"/>
+    <BaseInput v-model="form.porte" placeholder="Porte (pequeno, médio, grande)"/>
+    <BaseInput v-model="form.raca" placeholder="Raça"/>
+    <BaseInput v-model="form.observacoes" placeholder="Observações"/>
+    <BaseInput v-model="form.foto" placeholder="URL da foto"/>
+
+    <BaseButton :label="labelButton" @click="emit('submit')"/>
   </div>
 </template>
 
