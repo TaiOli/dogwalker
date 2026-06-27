024a4f7c24a4456eb55097ad9c7be9aea7d85ae4
Tais <taiscoliveiratec@gmail.com>
2026-06-27 14:40:42 -0300

    feat: arquivo para receber foto

---
+++ frontend/src/utils/image.js
@@ -0,0 +1,7 @@
+export const getFoto = (foto) => {
+  if (!foto) return "https://placehold.co/120x120"
+
+  if (foto.startsWith("http")) return foto
+
+  return `http://localhost:8000/storage/${foto}`
+}
