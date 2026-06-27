5bf29a6f12b18afa6a6e00f7e6783574e32cd3d2
Tais <taiscoliveiratec@gmail.com>
2026-06-27 14:36:24 -0300

    feat: corrige create paraaceitar foto e service de acordo com controller

--- backend/app/Services/UserService.php
+++ backend/app/Services/UserService.php
@@ -9,6 +9,10 @@ class UserService
 {
     public function create(array $data)
     {
+        if (isset($data['foto'])) {
+            $data['foto'] = $data['foto']->store('users', 'public');
+        }
+
         $data['password'] = Hash::make($data['password']);
 
         return User::create($data);
@@ -28,5 +32,17 @@ class UserService
             'user' => $user,
             'token' => $token
         ];
+    }
+
+    public function walkers()
+    {
+        return User::where('tipo_usuario', 'passeador')
+            ->select('id', 'nome', 'telefone', 'foto')
+            ->get();
+    }
+
+    public function show($id)
+    {
+        return User::findOrFail($id);
     }
 }
