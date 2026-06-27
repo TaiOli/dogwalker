216c620b093205dcae76049e80e63d8c98478989
Tais <taiscoliveiratec@gmail.com>
2026-06-27 14:37:30 -0300

    feat: adiciona rota para foto, lista de passeadores e busca de usuario

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,12 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DogController;
 use App\Http\Controllers\PasseioController;
+use Illuminate\Http\Request;
 
 Route::post('/users', [UserController::class, 'store']);
 Route::post('/login', [UserController::class, 'login']);
 
 // Protegidas
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/walkers', [UserController::class, 'walkers']);
+    Route::get('/walkers/{id}', [UserController::class, 'show']);
     Route::post('/dogs', [DogController::class, 'store']);
     Route::get('/dogs/my', [DogController::class, 'myDogs']);
     Route::post('/passeios', [PasseioController::class, 'store']);
@@ -18,4 +21,16 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::patch('/passeios/{id}/recusar', [PasseioController::class, 'recusar']);
     Route::get('/meus-passeios',[PasseioController::class, 'meusPasseios']);
     Route::delete('/passeios/{id}', [PasseioController::class, 'destroy']);
+    Route::get('/me', function (Request $request) {
+        $user = $request->user();
+
+        return [
+            'id' => $user->id,
+            'nome' => $user->nome,
+            'email' => $user->email,
+            'telefone' => $user->telefone,
+            'tipo_usuario' => $user->tipo_usuario,
+            'foto' => $user->foto
+        ];
+    });
 });
