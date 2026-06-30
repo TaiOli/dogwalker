beb3fb4cf0d1df617ed466d79306d203a2be831d
Tais <tais.oliveira@prodeb.ba.gov.br>
2026-06-30 16:49:30 -0300

    feat:separa do controller as validacoes e adiciona no request

---
+++ backend/app/Http/Requests/StoreAvaliacaoRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AvaliacaoRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'passeio_id' => 'required|exists:passeios,id',
+            'nota' => 'required|integer|min:1|max:5',
+            'comentario' => 'nullable|string',
+            'tipo_avaliador' => 'required|in:tutor,passeador',
+        ];
+    }
+}
