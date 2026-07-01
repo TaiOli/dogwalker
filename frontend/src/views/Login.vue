9838cffd73850d509399599c54f8ede8ec75bd06
Tais <tais.oliveira@prodeb.ba.gov.br>
2026-07-01 11:39:34 -0300

    fix: restaurei o login.vue

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container-fluid min-vh-100 d-flex justify-content-center align-items-center bg-light">
+    
+    <div class="login-card shadow p-4">
+      
+      <div class="card-body text-center">
+
+        <h2 class="mb-3">🐶 Dog Walker</h2>
+
+        <p class="text-muted mb-4">
+          Entrar no sistema
+        </p>
+
+        <UserAuthForm
+          :form="formLogin"
+          labelButton="Entrar"
+          @submit="acessoLogin"
+        />
+
+        <p class="mt-3 text-muted">
+          Não tem conta?
+          <router-link to="/cadastro-usuario" class="text-decoration-none fw-bold">
+            Criar conta
+          </router-link>
+        </p>
+
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import { useAuth } from "../composables/userAuth";
+import UserAuthForm from "../components/molecules/UserAuthForm.vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const { formLogin, login, clearLogin } = useAuth();
+
+async function acessoLogin() {
+  try {
+    const data = await login();
+
+    alert("Login realizado com sucesso!");
+
+    localStorage.setItem("user", JSON.stringify(data.user));
+    localStorage.setItem("token", data.token);
+
+    clearLogin();
+
+    router.push("/home");
+  } catch (error) {
+    console.log(error);
+    alert("Email ou senha inválidos");
+  }
+}
+</script>
+
+<style scoped>
+.login-card {
+  width: 100%;
+  max-width: 550px;
+}
+</style>
