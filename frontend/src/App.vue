375a246fb93f3679b6161bf8d1365b305790b99f
Tais <tais.oliveira@prodeb.ba.gov.br>
2026-07-01 16:23:24 -0300

    fix:remove css que nao estava sendo utilizado

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -27,17 +27,6 @@ const isLoginPage = computed(() =>
 </template>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-html, body, #app {
-  width: 100%;
-  height: 100%;
-}
-
 .layout {
   display: flex;
   width: 100%;
