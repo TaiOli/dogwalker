e2e97ca9e75404ada55c3ac08496d08b1d09489f
Tais <tais.oliveira@prodeb.ba.gov.br>
2026-07-15 15:46:14 -0300

    feat: adiciona no register do app service provider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Services\Contracts\UserServiceInterface;
+use App\Services\UserService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+        UserServiceInterface::class,
+        UserService::class
+    );
     }
 
     /**
