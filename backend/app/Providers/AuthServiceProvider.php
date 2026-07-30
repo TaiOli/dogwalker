4a38518c29650b90fe13cb2812934b0c5d73f8df
Tais <tais.oliveira@prodeb.ba.gov.br>
2026-07-30 09:30:32 -0300

    feat:registra policies user e dog

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -2,14 +2,20 @@
 
 namespace App\Providers;
 
+use App\Models\Dog;
 use App\Models\Tour;
+use App\Models\User;
+use App\Policies\DogPolicy;
 use App\Policies\TourPolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         Tour::class => TourPolicy::class,
+        User::class => UserPolicy::class,
+        Dog::class => DogPolicy::class,
     ];
 
     public function boot(): void
